033016cd15c62154b3f65dc1cc877295b2b5164c
JacobTate <jacob87608@gmail.com>
2020-06-11 10:48:09 -0500

    updating the state with form values on the signup page

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
 import { Container, Button } from "reactstrap";
 class SignUpForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        };
+    };
   handeChange = (event) => {
     const inputName = event.target.name;
+    const inputValue = event.target.value;
     switch (inputName) {
       case "firstName":
-          
+          this.setState({firstName: inputValue});
         break;
       case "lastName":
-        
+        this.setState({lastName: inputValue});
         break;
       case "email":
-        
+        this.setState({email: inputValue});
         break;
       case "password":
-        
+        this.setState({password: inputValue});
         break;
-    }
+    };
   };
   render() {
     return (
