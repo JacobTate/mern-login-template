cbf12c4ce68ac11c601dd5158b1e378cb3872956
JacobTate <jacob87608@gmail.com>
2020-06-11 10:42:30 -0500

    added function to get the data from the signup from

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container, Button } from "reactstrap";
-function SignUpForm(props) {
-  return (
-    <div>
-      <Container>
-        <form>
-          <input type="text" required name="firstName"></input>
-          <input type="text" required name="lastName"></input>
-          <input type="email" required name="email"></input>
-          <input type="password" required name="password"></input>
-          <Button color="primary">Submit</Button>
-        </form>
-      </Container>
-    </div>
-  );
+class SignUpForm extends Component {
+  handeChange = (event) => {
+    const inputName = event.target.name;
+    switch (inputName) {
+      case "firstName":
+          
+        break;
+      case "lastName":
+        
+        break;
+      case "email":
+        
+        break;
+      case "password":
+        
+        break;
+    }
+  };
+  render() {
+    return (
+      <div>
+        <Container>
+          <form>
+            <input
+              onChange={(event) => {
+                this.handeChange(event);
+              }}
+              type="text"
+              required
+              name="firstName"
+            ></input>
+            <input
+              onChange={(event) => {
+                this.handeChange(event);
+              }}
+              type="text"
+              required
+              name="lastName"
+            ></input>
+            <input
+              onChange={(event) => {
+                this.handeChange(event);
+              }}
+              type="email"
+              required
+              name="email"
+            ></input>
+            <input
+              onChange={(event) => {
+                this.handeChange(event);
+              }}
+              type="password"
+              required
+              name="password"
+            ></input>
+            <Button color="primary">Submit</Button>
+          </form>
+        </Container>
+      </div>
+    );
+  }
 }
 export default SignUpForm;
